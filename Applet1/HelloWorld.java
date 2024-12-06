09848aeb6afea9a78b679313f2857dcfbb2b3224
Anushka Sudhakar Karade <162126303+anushkaskarade@users.noreply.github.com>
2024-12-06 12:45:45 -0500

    Add files via upload

---
+++ Applet1/HelloWorld.java
@@ -0,0 +1,12 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class HelloWorld extends Applet
+{
+   
+      public void paint(Graphics g)
+      {
+         g.drawString("Hello World!",50,50);
+       }
+   
+}   
