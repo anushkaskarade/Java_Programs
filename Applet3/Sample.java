338432b61395aff42912de98672221e4486dd5ee
Anushka Sudhakar Karade <162126303+anushkaskarade@users.noreply.github.com>
2024-12-10 12:18:23 -0500

    Add files via upload

--- Applet3/Sample.java
+++ Applet3/Sample.java
@@ -1,1 +1,26 @@
+import java.applet.*;
+import java.awt.*;
 
+public class Sample extends Applet{
+
+   String msg;
+   public void init()
+   {
+       setBackground(Color.ORANGE);
+       setForeground(Color.BLACK);
+       msg="Inside inti()....";
+   }
+
+   public void start()
+   {
+    msg+="Inside start()....";
+   }
+
+   public void paint(Graphics g)
+   {
+       msg+="Inside paint()....";
+       g.drawString(msg,10,30);
+   }
+
+
+}
