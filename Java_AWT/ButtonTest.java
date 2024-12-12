180fb0ef65f47a74ee4cf453983af4ed35093121
Anushka Sudhakar Karade <162126303+anushkaskarade@users.noreply.github.com>
2024-12-12 12:32:25 -0500

    Add files via upload

--- Java_AWT/ButtonTest.java
+++ Java_AWT/ButtonTest.java
@@ -1,1 +1,15 @@
+import java.applet.Applet;
+import java.awt.*;
 
+public class ButtonTest extends Applet {
+    
+    public void init()
+    {
+        Button b1,b2;
+        b1=new Button("Welcome");
+        add(b1);
+        b2=new Button("");
+        add(b2);
+    }
+
+}
