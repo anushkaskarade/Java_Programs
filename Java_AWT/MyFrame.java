897057583efe85f299c130595ec4f1336a8584ea
Anushka Sudhakar Karade <162126303+anushkaskarade@users.noreply.github.com>
2024-12-21 12:30:58 -0500

    Add files via upload

---
+++ Java_AWT/MyFrame.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+public class MyFrame
+{
+   public static void main(String[] args)
+  {
+     Frame frame=new Frame();
+     frame.setTitle("Frame in Java");
+     frame.setSize(500,500);
+     frame.setBackground(Color.blue);
+     frame.setVisible(true);
+
+  }
+}
