3d72375129d8256bc417b02b944ddb8a5ad2a8cc
Anushka Sudhakar Karade <162126303+anushkaskarade@users.noreply.github.com>
2024-12-25 12:27:46 -0500

    Add files via upload

--- Java_Swing/Button.java
+++ Java_Swing/Button.java
@@ -1,1 +1,17 @@
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Button{
+    public static void main(String[] args)
+    {
+       JFrame f=new JFrame("Button Example")  ;
+       JButton b=new JButton("Click me!");
+       b.setBounds(50,100,95,30);
+       f.add(b);
+       f.setSize(400,400);
+       f.setLayout(null);
+       f.setVisible(true);
+
+    }
+}
